0b8301da903fa0e9fc915bf6ba56a4a3b5dd482d
tray190 <belltracy411@gmail.com>
2021-12-11 00:26:32 -0600

    Method Overloading

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x,  decimal y)
+        {
+            return (int)x + y;
+        }
+        public static string Add(int x, int y, bool drink)
+        {
+            if (drink && x + y != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (drink && x + y == 1) ; 
+            {
+                return $"{x + y} dollar";
+            }
+            else
+
+            { return x + y.ToString(); 
+
+            }
+
+            
+            } 
+        }
+}
